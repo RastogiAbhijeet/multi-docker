f7ce4a6058f4eafb75b1edce05c6e1444a502e14
RastogiAbhijeet <abhijeet.rastogi@block8.io>
2019-06-17 19:55:51 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Fib from "./Fib";
 function App() {
   return (
     <BrowserRouter>
+      <h1>Fib Calculator</h1>
       <div className="App">
         <Link to="/">Home</Link>
         <Link to="/otherpage">otherPage</Link>
